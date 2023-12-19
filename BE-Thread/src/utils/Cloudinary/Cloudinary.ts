218a487c1ad9f706c94723bcf48e9710e0fdab9e
roubilibo <roubilridlo@gmail.com>
2023-12-19 14:38:29 +0700

    env

--- BE-Thread/src/utils/Cloudinary/Cloudinary.ts
+++ BE-Thread/src/utils/Cloudinary/Cloudinary.ts
@@ -6,7 +6,7 @@ export const uploadToCloudinary = (
 ): Promise<string> => {
 	cloudinary.config({
 		cloud_name: Env.CLOUDINARY_CLOUD_NAME,
-		api_key: Env.CLOUDINARY_API_SECRET,
+		api_key: Env.CLOUDINARY_API_KEY,
 		api_secret: Env.CLOUDINARY_API_SECRET,
 	});
 
